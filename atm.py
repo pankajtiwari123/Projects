acb9bf1429be65f5b772c24d6d5b83f16d54a2d1
Pankaj Tiwari <tiwaripankaj8510@gmail.com>
2026-03-23 16:50:31 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -8,7 +8,7 @@ transactions = []
 def check_balance():
   st.write("Current Balance:", balance)
 def deposit_money():
-  amount =number_input("Enter deposit amount: ")
+  amount =text_input("Enter deposit amount: ")
   if amount > 0:
     balance += amount
     transactions.append(f"Deposited: {amount}")
